f0f20b3ad2cb59665e162b37a1eb56a5f903715b
Ian Kim <me@ian.kim>
2020-11-28 15:52:08 -0500

    Use TypeScript, eslint, and yarn

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Home(): React.ReactElement {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
+}
+export default Home;
