5181753fdd6f300b65af4184dff166798f23c682
Ian Kim <me@ian.kim>
2020-12-19 18:34:37 -0500

    Use useStaticQuery hook instead of component

--- src/pages/arknights/index.tsx
+++ src/pages/arknights/index.tsx
@@ -1,40 +1,36 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 
 function Home(): React.ReactElement {
-  return (
-    <StaticQuery
-      query={graphql`
-        query {
-          allOperatorsJson {
-            nodes {
-              name
-              rarity
-              isCnOnly
-            }
-          }
+  const data = useStaticQuery(graphql`
+    query {
+      allOperatorsJson {
+        nodes {
+          name
+          rarity
+          isCnOnly
         }
-      `}
-      render={(data) => (
-        <>
-          {data.allOperatorsJson.nodes.map((node) => (
-            <div>
-              <h3>{node.name}</h3>
-              <ul>
-                <li>
-                  Rarity:&nbsp;
-                  {node.rarity}
-                </li>
-                <li>
-                  CN Only?&nbsp;
-                  {node.isCnOnly ? "true" : "false"}
-                </li>
-              </ul>
-            </div>
-          ))}
-        </>
-      )}
-    />
+      }
+    }
+  `);
+  return (
+    <>
+      {data.allOperatorsJson.nodes.map((node) => (
+        <div>
+          <h3>{node.name}</h3>
+          <ul>
+            <li>
+              Rarity:&nbsp;
+              {node.rarity}
+            </li>
+            <li>
+              CN Only?&nbsp;
+              {node.isCnOnly ? "true" : "false"}
+            </li>
+          </ul>
+        </div>
+      ))}
+    </>
   );
 }
 export default Home;
