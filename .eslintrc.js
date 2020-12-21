51d7801e54697c9f7f1e357e154f0f479b457618
Ian Kim <me@ian.kim>
2020-12-20 21:03:28 -0500

    Add eslintrc settings from arknights-materials-checklist

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,53 @@
 module.exports = {
-  extends: ["airbnb-typescript", "prettier", "prettier/@typescript-eslint"],
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: ["./tsconfig.json"],
   },
-  ignorePatterns: [".eslintrc.js"]
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        'argsIgnorePattern': '^_',
+      },
+    ],
+    'react/jsx-no-duplicate-props': [
+      'error',
+      {
+        ignoreCase: false,
+      },
+    ],
+    'react/require-default-props': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-restricted-syntax': [ 
+      'error', 
+      { 
+        selector: 'ForInStatement', 
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.', 
+      }, 
+      { 
+        selector: 'LabeledStatement', 
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.', 
+      }, 
+      { 
+        selector: 'WithStatement', 
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.', 
+      }, 
+    ],
+  }
 };
