53aec8c46c72a7a5dffece5c65e96058e03c0e97
Ian Kim <iansjk@gmail.com>
2020-12-24 16:19:43 -0500

    Add typing for operators.json

--- src/pages/arknights/planner/types.ts
+++ src/pages/arknights/planner/types.ts
@@ -9,3 +9,45 @@ export interface Ingredient {
   tier: number;
   quantity: number;
 }
+
+interface Operator {
+  name: string;
+  rarity: number;
+  isCnOnly: boolean;
+  elite: OperatorEliteGoal[];
+  skillLevels: OperatorSkillLevelGoal[];
+  skills: OperatorSkill;
+}
+
+interface OperatorSkill {
+  slot: number;
+  skillId: string;
+  iconId: string | null;
+  skillName: string;
+  masteries: OperatorMasteryGoal[];
+}
+
+enum OperatorGoalCategory {
+  "Elite" = 0,
+  "Mastery",
+  "Skill Level",
+}
+
+export interface OperatorGoal {
+  goalName: string;
+  goalShortName: string;
+  goalCategory: OperatorGoalCategory;
+  ingredients: Ingredient[];
+}
+
+export type OperatorEliteGoal = OperatorGoal & {
+  eliteLevel: number;
+};
+
+export type OperatorSkillLevelGoal = OperatorGoal & {
+  skillLevel: number;
+};
+
+export type OperatorMasteryGoal = OperatorGoal & {
+  masteryLevel: number;
+};
