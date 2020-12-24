91ad55de8b5312f6c3fe0b3dec40c56a92e027d9
Ian Kim <iansjk@gmail.com>
2020-12-23 23:20:47 -0500

    Apply prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,56 @@
 module.exports = {
   extends: [
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/react',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
-  plugins: ['react', '@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
+  plugins: ["react", "@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: ["./tsconfig.json"],
   },
   ignorePatterns: [".eslintrc.js"],
   rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
       {
-        'argsIgnorePattern': '^_',
+        argsIgnorePattern: "^_",
       },
     ],
-    'react/jsx-no-duplicate-props': [
-      'error',
+    "react/jsx-no-duplicate-props": [
+      "error",
       {
         ignoreCase: false,
       },
     ],
-    'react/require-default-props': 'off',
-    'prettier/prettier': [
-      'error',
+    "react/require-default-props": "off",
+    "prettier/prettier": [
+      "error",
       {
-        endOfLine: 'auto',
+        endOfLine: "auto",
       },
     ],
-    'no-restricted-syntax': [ 
-      'error', 
-      { 
-        selector: 'ForInStatement', 
-        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.', 
-      }, 
-      { 
-        selector: 'LabeledStatement', 
-        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.', 
-      }, 
-      { 
-        selector: 'WithStatement', 
-        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.', 
-      }, 
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "ForInStatement",
+        message:
+          "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
+      },
+      {
+        selector: "LabeledStatement",
+        message:
+          "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+      },
+      {
+        selector: "WithStatement",
+        message:
+          "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
+      },
     ],
-  }
+  },
 };
