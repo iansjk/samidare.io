645d06acbd47f2a648e15f95d347750635c4189b
Ian Kim <iansjk@gmail.com>
2021-01-12 23:31:25 -0500

    Manually add "encoding" module
    Due to https://github.com/netlify/zip-it-and-ship-it/issues/68, and because
    faunadb uses node-fetch, we have to explicitly add encoding as a dependency or
    the function fails to compile on Netlify.

--- src/functions/planner-state.ts
+++ src/functions/planner-state.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import { APIGatewayEvent } from "aws-lambda";
 import faunadb, { query as q } from "faunadb";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import encoding from "encoding";
 import { Item, OperatorGoal } from "../types";
 
 const client = new faunadb.Client({
