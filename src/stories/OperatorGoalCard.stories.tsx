1d57ed0111fdecd2ba6e4bf1069bdb816614b76e
Ian Kim <iansjk@gmail.com>
2021-05-05 17:48:50 -0400

    Remove unused import

--- src/stories/OperatorGoalCard.stories.tsx
+++ src/stories/OperatorGoalCard.stories.tsx
@@ -4,7 +4,6 @@ import { Grid } from "@material-ui/core";
 import OperatorGoalCard, {
   OperatorGoalCardProps,
 } from "../components/OperatorGoalCard";
-import { OperatorGoalCategory } from "../types";
 
 export default {
   title: "Planner/OperatorGoalCard",
