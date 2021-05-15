05505652a7d668c31fb44c98a3728c67937de0a6
Ian Kim <iansjk@gmail.com>
2021-05-14 23:20:49 -0400

    Make goal adding a little less flaky

--- cypress/integration/common/steps.ts
+++ cypress/integration/common/steps.ts
@@ -2,6 +2,7 @@ import { When } from "cypress-cucumber-preprocessor/steps";
 
 When(/^I (?:have )?add(?:ed)? a goal to my planner$/, () => {
   cy.get('input[name="operator-name"]').type("Amiya{enter}");
+  cy.wait(100);
   cy.get("#goal-select").click();
   cy.get('li[role="option"]').contains("Elite 2").click().type("{esc}");
   cy.contains("Add").click();
