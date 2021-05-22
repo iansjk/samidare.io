360fcac7063066043bb9c47c5915c3050ae577bc
Ian Kim <iansjk@gmail.com>
2021-05-21 21:09:09 -0400

    Mock up page layout

--- src/pages/leveling.tsx
+++ src/pages/leveling.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
+import { useStaticQuery, graphql } from "gatsby";
 import {
   eliteLmdCost,
   expCostByElite,
   lmdCostByElite,
   maxLevelByRarity,
 } from "../data/leveling.json";
+import { Operator } from "../types";
 
 interface LevelingCost {
   xp: number;
@@ -18,9 +29,86 @@ function levelingCost(
   startingLevel: number,
   targetElite: number,
   targetLevel: number
-): LevelingCost {}
+): LevelingCost {
+  return {
+    xp: 0,
+    lmd: 0,
+    levelingLmd: 0,
+    eliteLmd: 0,
+  };
+}
 
 const Leveling: React.FC = () => {
-  return <></>;
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allOperatorsJson(sort: { fields: name, order: ASC }) {
+          nodes {
+            name
+            rarity
+          }
+        }
+      }
+    `
+  );
+  const operators: Operator[] = data.allOperatorsJson.nodes;
+  const [operatorName, setOperatorName] = useState<string | null>(null);
+  const [startingElite, setStartingElite] = useState(0);
+  const [startingLevel, setStartingLevel] = useState(1);
+  const [targetElite, setTargetElite] = useState(0);
+  const [targetLevel, setTargetLevel] = useState(1);
+  const operator = operators.find((op) => op.name === operatorName);
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <Autocomplete
+          options={operators.map((op) => op.name)}
+          autoComplete
+          autoHighlight
+          value={operatorName}
+          onChange={(_, value) => setOperatorName(value)}
+          id="operator-name"
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              name="operator-name"
+              label="Operator name"
+              variant="outlined"
+            />
+          )}
+        />
+        <Card>
+          <CardContent>
+            <Typography>Start</Typography>
+            Starting elite
+            <br />
+            Starting level
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent>
+            <Typography>End</Typography>
+            Target elite
+            <br />
+            Target level
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={6}>
+        <Card>
+          <CardContent>
+            Total LMD cost:
+            <br />
+            Total XP cost:
+            <br />
+            LMD cost for leveling:
+            <br />
+            LMD cost for elite promotions:
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 };
 export default Leveling;
