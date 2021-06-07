0de9aa8a76a0e9aedb454b879d6837c061aa3f95
Ian Kim <iansjk@gmail.com>
2021-06-07 00:54:44 -0400

    Move enterOperatorName into common/utils

--- cypress/integration/features/planner/utils.ts
+++ cypress/integration/features/planner/utils.ts
@@ -1,8 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-export function enterOperatorName(operatorName: string): Cypress.Chainable {
-  cy.get("#operator-name").type(`${operatorName}{enter}`);
-  return cy.wait(300);
-}
+import { enterOperatorName } from "../../common/utils";
 
 export function addGoal(
   operatorName: string,
