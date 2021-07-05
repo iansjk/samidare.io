7db7d64e7e217cc5e55f845e1d4964921034850c
Ian Kim <iansjk@gmail.com>
2021-07-05 12:53:47 -0400

    Fix width for a single base snapshot

--- src/pages/base.tsx
+++ src/pages/base.tsx
@@ -4,9 +4,15 @@ import SwipeableViews from "react-swipeable-views";
 import BaseComponent, { Rotation } from "../components/base/Base";
 import rotationsJson from "./rotations.json";
 
+const containerStyle = {
+  display: "grid",
+  gridAutoFlow: "column",
+  gridAutoColumns: "100%",
+};
+
 const Base: React.FC = () => {
   return (
-    <SwipeableViews enableMouseEvents>
+    <SwipeableViews enableMouseEvents containerStyle={containerStyle}>
       {rotationsJson.rotations.map((rotation, i) => (
         // eslint-disable-next-line react/jsx-props-no-spreading
         <BaseComponent key={i} {...(rotation as Rotation)} />
